63e8a9d423556b977727486d5457ef11cbce3d46
alextperry <alextperry@outlook.com>
2025-03-28 12:54:09 -0600

    ran project

--- backend/Bookstore/Bookstore/obj/Debug/net8.0/Bookstore.AssemblyInfo.cs
+++ backend/Bookstore/Bookstore/obj/Debug/net8.0/Bookstore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bookstore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7769bc890f1b60948eeb14433d5094d275dc0ab0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bookstore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bookstore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
